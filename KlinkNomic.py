0a5889afa1b7516eea8e86f8691fbffa7c83ccd5
klinkplink <47506135+klinkplink@users.noreply.github.com>
2021-09-09 10:14:59 -0700

    Completed hand sorting
    this can now sort by rank, suit, color, and tradition. Rolled card stuff into classes to make it easier to work with cards

--- KlinkNomic.py
+++ KlinkNomic.py
@@ -4,72 +4,172 @@ from collections import deque
 import re
 
 CB = Tk()
-
-suitdict = {
-    1: "Leaves",
-    2: "Diamonds",
-    3: "Cups",
-    4: "Clubs",
-    5: "Acorns",
-    6: "Roses",
-    7: "Bells",
-    8: "Swords",
-    9: "Shields",
-    10: "Hearts",
-    11: "Coins",
-    12: "Spades",
-    13: "Tarot"
-}
-
-rankdict = {
-    1: "Ace",
-    2: "2",
-    3: "3",
-    4: "4",
-    5: "5",
-    6: "6",
-    7: "7",
-    8: "8",
-    9: "9",
-    10: "10",
-    11: "11",
-    12: "12",
-    13: "13",
-    14: "Unter Knave",
-    15: "Ober Knave",
-    16: "Knight",
-    17: "Bishop",
-    18: "Rook",
-    19: "Queen",
-    20: "King"
-}
+CB.withdraw()
 
 
-tarotdict = {
-    1: "The Fool",
-    2: "The Magician",
-    3: "The High Priestess",
-    4: "The Empress",
-    5: "The Emperor",
-    6: "The Hierophant",
-    7: "The Lovers",
-    8: "The Chariot",
-    9: "Justice",
-    10: "The Hermit",
-    11: "Wheel of Fortune",
-    12: "Strength",
-    13: "The Hanged Man",
-    14: "Death",
-    15: "Temperance",
-    16: "The Devil",
-    17: "The Tower",
-    18: "The Star",
-    19: "The Moon",
-    20: "The Sun",
-    21: "Judgement",
-    22: "The World"
-}
+class PlayingCard:
+    def __init__(self):
+        self.suitnum = 0
+        self.ranknum = 0
+        self.suit = ""
+        self.rank = ""
+        self.color = ""
+        self.tradition = ""
 
+    suit_alpha = {
+        "L": 1,
+        "D": 2,
+        "Cp": 3,
+        "C": 4,
+        "A": 5,
+        "R": 6,
+        "B": 7,
+        "Sw": 8,
+        "Sh": 9,
+        "H": 10,
+        "Cn": 11,
+        "S": 12
+    }
+
+    rank_alpha =   {
+        "A": 1,
+        "T": 10,
+        "E": 11,
+        "D": 12,
+        "H": 13,
+        "U": 14,
+        "O": 15,
+        "N": 16,
+        "B": 17,
+        "R": 18,
+        "Q": 19,
+        "K": 20
+    }
+
+    card_suits = {
+        1: "Leaves",
+        2: "Diamonds",
+        3: "Cups",
+        4: "Clubs",
+        5: "Acorns",
+        6: "Roses",
+        7: "Bells",
+        8: "Swords",
+        9: "Shields",
+        10: "Hearts",
+        11: "Coins",
+        12: "Spades",
+        13: "Tarot"
+    }
+
+    card_ranks = {
+        1: "Ace",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9",
+        10: "10",
+        11: "11",
+        12: "12",
+        13: "13",
+        14: "Unter Knave",
+        15: "Ober Knave",
+        16: "Knight",
+        17: "Bishop",
+        18: "Rook",
+        19: "Queen",
+        20: "King"
+    }
+
+    card_colors = {
+        1: "Green",
+        2: "Red",
+        3: "Gold",
+        4: "Black",
+        5: "Green",
+        6: "Red",
+        7: "Gold",
+        8: "Black",
+        9: "Green",
+        10: "Red",
+        11: "Gold",
+        12: "Black",
+        13: "Special"
+
+    }
+
+    card_traditions = {
+        1: "German",
+        2: "French",
+        3: "Spanish",
+        4: "Spanish / French",
+        5: "German / Swiss",
+        6: "Swiss",
+        7: "German / Swiss",
+        8: "Spanish",
+        9: "Swiss",
+        10: "French / German",
+        11: "Spanish",
+        12: "French",
+        13: "Tarot"
+    }
+
+    def interpret(self):
+        try:
+            self.suit = self.card_suits[int(self.suitnum)]
+            self.suitnum = int(self.suitnum)
+            self.color = self.card_colors[int(self.suitnum)]
+            self.tradition = self.card_traditions[int(self.suitnum)]
+        except ValueError:
+            self.suit = self.card_suits[self.suit_alpha[self.suitnum]]
+            self.suitnum = int(self.suit_alpha[self.suitnum])
+            self.color = self.card_colors[self.suitnum]
+            self.tradition = self.card_traditions[self.suitnum]
+
+        try:
+            self.rank = self.card_ranks[int(self.ranknum)]
+            self.ranknum = int(self.ranknum)
+        except ValueError:
+            self.rank = self.card_ranks[self.rank_alpha[self.ranknum]]
+            self.ranknum = int(self.rank_alpha[self.ranknum])
+
+class TarotCard:
+    tarot_ranks = {
+        1: "The Fool",
+        2: "The Magician",
+        3: "The High Priestess",
+        4: "The Empress",
+        5: "The Emperor",
+        6: "The Hierophant",
+        7: "The Lovers",
+        8: "The Chariot",
+        9: "Justice",
+        10: "The Hermit",
+        11: "Wheel of Fortune",
+        12: "Strength",
+        13: "The Hanged Man",
+        14: "Death",
+        15: "Temperance",
+        16: "The Devil",
+        17: "The Tower",
+        18: "The Star",
+        19: "The Moon",
+        20: "The Sun",
+        21: "Judgement",
+        22: "The World"
+    }
+    def __init__(self):
+        self.ranknum = ""
+        self.rank = ""
+    def interpret(self):
+        self.rank = self.tarot_ranks[int(self.ranknum)]
+
+def testfunction():
+    pass
 
 def createplayerlist():
     clipboardData = CB.clipboard_get()
@@ -83,7 +183,7 @@ def cleaninventory(unclean):
     return clean
 
 def processtaxes():
-    #make sure the wiki table is in your clipboard starting with thhe first player
+    #make sure the wiki table is in your clipboard starting with the first player
     print("1: All Players | 2: Players with tax < 100")
     whichmode = input()
     try:
@@ -131,16 +231,16 @@ def processtaxes():
         elif int(whichmode) == 2 and TaxValue < 100:
             print(PlayerName + NameBuffer + "| New Chips:" + buffer(NewChipValue) + "Wealth:" + buffer(WealthValue) + "Chips:" + buffer(
                 ChipValue) + "Trungs:" + buffer(TrungValue) + "Tax:" + buffer(TaxValue) + "Gain:" + buffer(100 - TaxValue))
-    sys.exit()
+    input("Press any key to close")
 
 def processdiceroles():
 
     #Separate the three sets of rolls with | and copy it into your clipboard
     clipboardData = CB.clipboard_get()
     carddetails = clipboardData.split("|")
-    suits = deque([suitdict[int(x.strip("[").strip().strip("]"))] for x in carddetails[0].split(",")])
-    regularranks = deque([rankdict[int(y.strip().strip("]").strip("["))] for y in carddetails[1].split(",")])
-    tarotranks = deque([tarotdict[int(z.strip().strip("]").strip("["))] for z in carddetails[2].split(",")])
+    suits = deque([PlayingCard.card_suits[int(x.strip("[").strip().strip("]"))] for x in carddetails[0].split(",")])
+    regularranks = deque([PlayingCard.card_ranks[int(y.strip().strip("]").strip("["))] for y in carddetails[1].split(",")])
+    tarotranks = deque([TarotCard.tarot_ranks[int(z.strip().strip("]").strip("["))] for z in carddetails[2].split(",")])
 
     assembledCards = deque([])
 
@@ -153,107 +253,75 @@ def processdiceroles():
     for x, value in enumerate(range(int(len(assembledCards) / 3))):
         print("Lot " + str(
             value + 1) + ": " + assembledCards.popleft() + ", " + assembledCards.popleft() + ", " + assembledCards.popleft())
-    sys.exit()
+    input("Press any button to exit")
 
 def processhand():
     GetPlayers = createplayerlist()
 
-    print("1: Sort by Rank | 2: Sort by Suit")
+    print("1: Sort by Rank | 2: Sort by Suit | 3: Sort by Color | 4: Sort by Tradition")
     whichsort = input()
     try:
         whichsort = int(whichsort)
     except ValueError:
         whichsort = 1
 
-    wikirankdict = {
-        "A": 1,
-        "T": 10,
-        "E": 11,
-        "D": 12,
-        "H": 13,
-        "U": 14,
-        "O": 15,
-        "N": 16,
-        "B": 17,
-        "R": 18,
-        "Q": 19,
-        "K": 20
-    }
-
-    wikisuitdict = {
-        "L": 1,
-        "D": 2,
-        "Cp": 3,
-        "C": 4,
-        "A": 5,
-        "R": 6,
-        "B": 7,
-        "Sw": 8,
-        "Sh": 9,
-        "H": 10,
-        "Cn": 11,
-        "S": 12
-    }
-
 
     for x in GetPlayers:
         CurrentPlayer = cleaninventory(x)
         def sortby():
-            CurrentHand = [z.strip("{").strip("}") for z in CurrentPlayer[3].split(" ") if z]
-            HandDetails = []
+            CurrentHand = [z.strip("{").strip("}").strip() for z in CurrentPlayer[3].split(" ") if z]
+            HandDetails =[]
             TarotSleeve = []
 
 
-            for z in CurrentHand:
-                cardtype = z.find("Card")
-                if cardtype > -1: # Activates if its a card
-                    cardrank = z[5]  # Rank
-                    cardsuit = wikisuitdict[z[7:]] # Suit converted into a number
-
-                    try:
-                        cardrank = int(cardrank)
-                    except ValueError:
-                        # if it cant int the rank, feeds it thru partial dictionary above to get the equivalent number
-                        cardrank = wikirankdict[cardrank]
-
-                    if len(HandDetails) == 0: #makes sure theres a card to compare to
-                        HandDetails.append({"Rank":cardrank,"Suit":cardsuit})
-                        continue
-
-
-                    if whichsort == 1:
-                        sorter = "Rank"
-                        valueholder = cardrank
-                    elif whichsort == 2:
-                        sorter = "Suit"
-                        valueholder = cardsuit
-
-                    Added = False
-                    for x in range(len(HandDetails)):
-                        if valueholder <= HandDetails[x][sorter]:
-                            HandDetails.insert(x,{"Rank":cardrank,"Suit":cardsuit})
-                            Added = True
-                            break
-                    if Added == False:
-                        HandDetails.append({"Rank": cardrank, "Suit": cardsuit})
+            for B in CurrentHand:
+                cardtype = B.find("Card")
+                if cardtype > -1: # add tarot to this
+                    HandDetails.append(PlayingCard())
+                    HandDetails[-1].ranknum = B[5]
+                    HandDetails[-1].suitnum = B[7:]
+                    HandDetails[-1].interpret()
                 else:
-                    #print("This is a Tarot")
-                    cardrank = int(z[6:])
-                    TarotSleeve.append(tarotdict[cardrank])
-                    pass
+                    TarotSleeve.append(TarotCard())
+                    TarotSleeve[-1].ranknum = B[6:]
+                    TarotSleeve[-1].interpret()
+
+            if whichsort == 1:
+                HandDetails.sort(key=lambda x: x.suitnum)
+                HandDetails.sort(key=lambda x: x.ranknum)
+            elif whichsort == 2:
+                HandDetails.sort(key=lambda x: x.ranknum)
+                HandDetails.sort(key=lambda x: x.suitnum)
+            elif whichsort == 3:
+                HandDetails.sort(key=lambda x: x.ranknum)
+                HandDetails.sort(key=lambda x: x.color)
+            elif whichsort == 4:
+                HandDetails.sort(key=lambda x: x.ranknum)
+                HandDetails.sort(key=lambda x: x.tradition)
+
+            HandPrint = CurrentPlayer[0] + ": "
             for x in HandDetails:
-                print(rankdict[x["Rank"]] + " of " + suitdict[x["Suit"]])
+                # add ability to print wiki formatted for rank / suit
+                HandPrint += x.rank + " " + x.suit
+                if whichsort == 3:
+                    HandPrint += " [" + x.color + "]"
+                elif whichsort == 4:
+                    HandPrint += + " [" + x.tradition + "]"
+                HandPrint += ", "
 
-            print("\nTarot Cards:")
+            if len(TarotSleeve) > 0:
+                HandPrint += "Tarot Cards: "
+                for x in TarotSleeve:
+                    HandPrint += "[" + x.rank + "]"
 
-            for x in TarotSleeve:
-                print(x)
+            print(HandPrint)
+        try:
+            if CurrentPlayer[3] != "-":
+                sortby()
+        except IndexError:
+            print("Make sure you copy the wiki table")
             return
-
-        if CurrentPlayer[3] != "-":
-            sortby()
-#            print(CurrentPlayer[0] + " " + CurrentPlayer[3])
-    sys.exit()
+    input("Press any key to close")
 
 while True:
     print("1: Tax | 2: Sort | 3: Hand")
@@ -269,5 +337,7 @@ while True:
         processdiceroles()
     elif int(whichprogram) == 3:
         processhand()
+    elif int(whichprogram) == 4:
+        testfunction()
     else:
         print("Thats not an option")
